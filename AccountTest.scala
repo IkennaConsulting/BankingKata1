ca901c19513bbded1ed871f64b21adbc5a754f9f
bjwealthy <bjwealthy@gmail.com>
2018-04-14 14:06:51 +0100

    test

---
+++ AccountTest.scala
@@ -0,0 +1,7 @@
+import org.scalatest.FunSuite
+
+class AcountTest extends Funsuite{
+  test("Account.deposit"){
+    assert((amountDepWithd > 0 && openingAmount == 5000) Account.deposit(200) === 5200)
+  }
+}git init
