5f2bb8fd715acd2f05c9fbb9794475e277a2a932
bjwealthy <bjwealthy@gmail.com>
2018-04-14 18:12:29 +0100

    test commit

--- AccountTest.scala
+++ AccountTest.scala
@@ -4,4 +4,8 @@ class AcountTest extends Funsuite{
   test("Account.deposit"){
     assert((amountDepWithd > 0 && openingAmount == 5000) Account.deposit(200) === 5200)
   }
-}git init
+
+  test("Account.withdrawal"){
+    assert((amountDepWithd > 0 && openingAmount == 5000) Account.withdrawal(200) === 4800)
+  }
+}
